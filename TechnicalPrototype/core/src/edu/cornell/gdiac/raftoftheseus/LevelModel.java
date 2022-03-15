913baf51fb1c6540589d94aab0922224197c6cab
Leo Zhao <lz527@cornell.edu>
2022-03-14 20:52:20 -0400

    update world controller

--- TechnicalPrototype/core/src/edu/cornell/gdiac/raftoftheseus/LevelModel.java
+++ TechnicalPrototype/core/src/edu/cornell/gdiac/raftoftheseus/LevelModel.java
@@ -1,4 +1,153 @@
 package edu.cornell.gdiac.raftoftheseus;
 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.JsonValue;
+import edu.cornell.gdiac.assets.AssetDirectory;
+import edu.cornell.gdiac.util.PooledList;
+
 public class LevelModel {
+
+    public GameObject raft;
+    public GameObject goal;
+    public AssetDirectory directory;
+    JsonValue level_data;
+    /** The Box2D world */
+    protected World world;
+    /** The boundary of the world */
+    protected Rectangle bounds;
+    /** The world scale */
+    protected Vector2 scale;
+    /** All the objects in the world. */
+    protected PooledList<GameObject> objects  = new PooledList<GameObject>();
+    /** Queue for adding objects */
+    protected PooledList<GameObject> addQueue = new PooledList<GameObject>();
+
+    public static final Vector2 NO_GRAVITY = new Vector2(0f ,0f );
+
+
+
+
+
+    /**
+     * Returns true if the object is in bounds.
+     *
+     * This assertion is useful for debugging the physics.
+     *
+     * @param obj The object to check.
+     *
+     * @return true if the object is in bounds.
+     */
+    public boolean inBounds(GameObject obj) {
+        boolean horiz = (bounds.x <= obj.getX() && obj.getX() <= bounds.x+bounds.width);
+        boolean vert  = (bounds.y <= obj.getY() && obj.getY() <= bounds.y+bounds.height);
+        return horiz && vert;
+    }
+
+    /**
+     *
+     * Adds a physics object in to the insertion queue.
+     *
+     * Objects on the queue are added just before collision processing.  We do this to
+     * control object creation.
+     *
+     * param obj The object to add
+     */
+    public void addQueuedObject(GameObject obj) {
+        assert inBounds(obj) : "Object is not in bounds";
+        addQueue.add(obj);
+    }
+
+    /**
+     * Immediately adds the object to the physics world
+     *
+     * param obj The object to add
+     */
+    protected void addObject(GameObject obj) {
+        assert inBounds(obj) : "Object is not in bounds";
+        objects.add(obj);
+        obj.activatePhysics(world);
+    }
+
+    /**
+     * Dispose of all (non-static) resources allocated to this mode.
+     */
+    public void dispose() {
+        for(GameObject obj : objects) {
+            obj.deactivatePhysics(world);
+        }
+        objects.clear();
+        addQueue.clear();
+        world.dispose();
+        objects = null;
+        addQueue = null;
+        bounds = null;
+        scale  = null;
+        world  = null;
+    }
+
+    /**
+     * Resets the status of the game so that we can play again.
+     * <p>
+     * This method disposes of the world and creates a new one.
+     */
+    public void reset() {
+        for(GameObject obj : objects) {
+            obj.deactivatePhysics(world);
+        }
+        objects.clear();
+        addQueue.clear();
+        world.dispose();
+
+        world = new World(NO_GRAVITY,false);
+
+    }
+
+
+    public void populateLevel() {
+        addCurrent(level_data.get("current"), objects);
+        addWood(level_data.get("wood"), objects);
+        addRocks(level_data.get("rocks"), objects);
+        objects.add(addRaft(level_data.get("raft")));
+        objects.add(addGoal(level_data.get("goal")));
+    }
+    public void setLevel(int level_int){
+        level_data = directory.getEntry("levels:" + level_int, JsonValue.class);
+        setWorld(level_data.get("world"), world);
+        populateLevel();
+    }
+
+    private GameObject addGoal(JsonValue goal) {
+        return null;
+    }
+
+    private GameObject addRaft(JsonValue raft) {
+        return null;
+    }
+
+    private void addRocks(JsonValue rocks, PooledList<GameObject> objs) {
+        for (JsonValue rock: rocks) {
+//            GameObject this_rock = new Rock(rock.get("x"), rock.get("y"));
+//            objs.add(this_rock);
+        }
+    }
+
+    private void addWood(JsonValue woods, PooledList<GameObject> objs) {
+        for (JsonValue wood: woods) {
+//            GameObject this_wood = new Wood(wood.get("x"), wood.get("y"), wood.get("value"));
+//            objs.add(this_wood);
+        }
+    }
+
+    private void addCurrent(JsonValue currents, PooledList<GameObject> objs) {
+        for (JsonValue current : currents) {
+//            GameObject this_current = new Current(current.get("x"), current.get("y"), current.get("direction"), current.get("speed"));
+//            objs.add(this_current);
+        }
+    }
+
+    private void setWorld(JsonValue world, World world1) {
+
+    }
 }
--- TechnicalPrototype/core/src/edu/cornell/gdiac/raftoftheseus/WorldController.java
+++ TechnicalPrototype/core/src/edu/cornell/gdiac/raftoftheseus/WorldController.java
@@ -2,15 +2,19 @@ package edu.cornell.gdiac.raftoftheseus;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
+import edu.cornell.gdiac.assets.AssetDirectory;
 import edu.cornell.gdiac.util.ScreenListener;
 import edu.cornell.gdiac.util.PooledList;
 
-public class WorldController implements Screen {
+import java.util.Iterator;
+
+public class WorldController implements Screen, ContactListener {
 
     // CONSTANTS
     /** Width of the game world in Box2d units */
@@ -20,14 +24,26 @@ public class WorldController implements Screen {
     /** The default value of gravity (going down) */
     protected static final float DEFAULT_GRAVITY = -4.9f;
 
+    /** Exit code for quitting the game */
+    public static final int EXIT_QUIT = 0;
+    /** Exit code for advancing to next level */
+    public static final int EXIT_NEXT = 1;
+    /** Exit code for jumping back to previous level */
+    public static final int EXIT_PREV = 2;
+    /** How many frames after winning/losing do we continue? */
+    public static final int EXIT_COUNT = 120;
+
+    /** The amount of time for a physics engine step. */
+    public static final float WORLD_STEP = 1/60.0f;
+    /** Number of velocity iterations for the constrain solvers */
+    public static final int WORLD_VELOC = 6;
+    /** Number of position iterations for the constrain solvers */
+    public static final int WORLD_POSIT = 2;
+
     // FIELDS
     // CANVAS AND OBJECT LIST
     /** Reference to the game canvas */
     protected GameCanvas canvas;
-    /** All the objects in the world. */
-    protected PooledList<GameObject> objects  = new PooledList<GameObject>();
-    /** Queue for adding objects */
-    protected PooledList<GameObject> addQueue = new PooledList<GameObject>();
     /** Listener that will update the player mode when we are done */
     private ScreenListener listener;
 
@@ -40,12 +56,9 @@ public class WorldController implements Screen {
     protected BitmapFont displayFont;
 
     // WORLD
-    /** The Box2D world */
-    protected World world;
-    /** The boundary of the world */
-    protected Rectangle bounds;
-    /** The world scale */
-    protected Vector2 scale;
+    protected LevelModel levelModel;
+
+    // LEVEL STATUS
     /** Whether or not this is an active controller */
     private boolean active;
     /** Whether we have completed this level */
@@ -80,9 +93,10 @@ public class WorldController implements Screen {
      * @param gravity	The gravitational force on this Box2d world
      */
     protected WorldController(Rectangle bounds, Vector2 gravity) {
-        this.world = new World(gravity,false);
-        this.bounds = new Rectangle(bounds);
-        this.scale = new Vector2(1,1);
+        levelModel = new LevelModel();
+        levelModel.world = new World(gravity,false);
+        levelModel.bounds = new Rectangle(bounds);
+        levelModel.scale = new Vector2(1,1);
         this.complete = false;
         this.failed = false;
         this.debug  = false;
@@ -111,8 +125,8 @@ public class WorldController implements Screen {
      */
     public void setCanvas(GameCanvas canvas) {
         this.canvas = canvas;
-        this.scale.x = canvas.getWidth()/bounds.getWidth();
-        this.scale.y = canvas.getHeight()/bounds.getHeight();
+//        this.scale.x = canvas.getWidth()/bounds.getWidth();
+//        this.scale.y = canvas.getHeight()/bounds.getHeight();
     }
 
 
@@ -130,7 +144,7 @@ public class WorldController implements Screen {
 //        canvas.clear();
 
         canvas.begin();
-        for(GameObject obj : objects) {
+        for(GameObject obj : levelModel.objects) {
             obj.draw(canvas);
         }
         canvas.end();
@@ -158,28 +172,219 @@ public class WorldController implements Screen {
     }
 
 
+    /**
+     * Gather the assets for this controller.
+     *
+     * This method extracts the asset variables from the given asset directory. It
+     * should only be called after the asset directory is completed.
+     *
+     * @param directory	Reference to global asset manager.
+     */
+    public void gatherAssets(AssetDirectory directory) {
+        // Allocate the tiles
+        earthTile = new TextureRegion(directory.getEntry( "shared:earth", Texture.class ));
+        goalTile  = new TextureRegion(directory.getEntry( "shared:goal", Texture.class ));
+        displayFont = directory.getEntry( "shared:retro" ,BitmapFont.class);
+        levelModel.directory = directory;
+    }
 
 
-    @Override
-    public void render(float delta) {
+    /**
+     * Returns whether to process the update loop
+     *
+     * At the start of the update loop, we check if it is time
+     * to switch to a new game mode.  If not, the update proceeds
+     * normally.
+     *
+     * @param dt	Number of seconds since last animation frame
+     *
+     * @return whether to process the update loop
+     */
+    public boolean preUpdate(float dt) {
+//        InputController input = InputController.getInstance();
+//        input.readInput(bounds, scale);
+//        if (listener == null) {
+//            return true;
+//        }
+//
+//        // Toggle debug
+//        if (input.didDebug()) {
+//            debug = !debug;
+//        }
+//
+//        // Handle resets
+//        if (input.didReset()) {
+//            reset();
+//        }
+//
+//        // Now it is time to maybe switch screens.
+//        if (input.didExit()) {
+//            pause();
+//            listener.exitScreen(this, EXIT_QUIT);
+//            return false;
+//        } else if (input.didAdvance()) {
+//            pause();
+//            listener.exitScreen(this, EXIT_NEXT);
+//            return false;
+//        } else if (input.didRetreat()) {
+//            pause();
+//            listener.exitScreen(this, EXIT_PREV);
+//            return false;
+//        } else if (countdown > 0) {
+//            countdown--;
+//        } else if (countdown == 0) {
+//            if (failed) {
+//                reset();
+//            } else if (complete) {
+//                pause();
+//                listener.exitScreen(this, EXIT_NEXT);
+//                return false;
+//            }
+//        }
+        return true;
+    }
+
+    /**
+     * Add a new bullet to the world and send it in the right direction.
+     */
+    private void createBullet() {
+//        JsonValue bulletjv = constants.get("bullet");
+//        float offset = bulletjv.getFloat("offset",0);
+//        offset *= (avatar.isFacingRight() ? 1 : -1);
+//        float radius = bulletTexture.getRegionWidth()/(2.0f*scale.x);
+//        WheelObstacle bullet = new WheelObstacle(avatar.getX()+offset, avatar.getY(), radius);
+//
+//        bullet.setName("bullet");
+//        bullet.setDensity(bulletjv.getFloat("density", 0));
+//        bullet.setDrawScale(scale);
+//        bullet.setTexture(bulletTexture);
+//        bullet.setBullet(true);
+//        bullet.setGravityScale(0);
+//
+//        // Compute position and velocity
+//        float speed = bulletjv.getFloat( "speed", 0 );
+//        speed  *= (avatar.isFacingRight() ? 1 : -1);
+//        bullet.setVX(speed);
+//        addQueuedObject(bullet);
+//
+//        fireId = playSound( fireSound, fireId );
+    }
+
+
+    /**
+     * The core gameplay loop of this world.
+     *
+     * This method contains the specific update code for this mini-game. It does
+     * not handle collisions, as those are managed by the parent class WorldController.
+     * This method is called after input is read, but before collisions are resolved.
+     * The very last thing that it should do is apply forces to the appropriate objects.
+     *
+     * @param dt	Number of seconds since last animation frame
+     */
+    public void update(float dt){
+//        // Process actions in object model
+//        levelModel.avatar.setMovement(InputController.getInstance().getHorizontal() * levelModel.avatar.getForce());
+//        levelModel.avatar.setJumping(InputController.getInstance().didPrimary());
+//        levelModel.avatar.setShooting(InputController.getInstance().didSecondary());
+//
+//        // Add a bullet if we fire
+//        if (levelModel.avatar.isShooting()) {
+//            createBullet();
+//        }
+//
+//        levelModel.avatar.applyForce();
+//        if (levelModel.avatar.isJumping()) {
+//            jumpId = playSound( jumpSound, jumpId, volume );
+//        }
 
     }
 
+    /**
+     * Processes physics
+     *
+     * Once the update phase is over, but before we draw, we are ready to handle
+     * physics.  The primary method is the step() method in world.  This implementation
+     * works for all applications and should not need to be overwritten.
+     *
+     * @param dt	Number of seconds since last animation frame
+     */
+    public void postUpdate(float dt) {
+        // Add any objects created by actions
+        while (!levelModel.addQueue.isEmpty()) {
+            levelModel.addObject(levelModel.addQueue.poll());
+        }
+
+        // Turn the physics engine crank.
+        levelModel.world.step(WORLD_STEP,WORLD_VELOC,WORLD_POSIT);
+
+        // Garbage collect the deleted objects.
+        // Note how we use the linked list nodes to delete O(1) in place.
+        // This is O(n) without copying.
+        Iterator<PooledList<GameObject>.Entry> iterator = levelModel.objects.entryIterator();
+        while (iterator.hasNext()) {
+            PooledList<GameObject>.Entry entry = iterator.next();
+            GameObject obj = entry.getValue();
+            if (obj.isRemoved()) {
+                obj.deactivatePhysics(levelModel.world);
+                entry.remove();
+            } else {
+                // Note that update is called last!
+                obj.update(dt);
+            }
+        }
+    }
+
+    /**
+     * Called when the Screen should render itself.
+     *
+     * We defer to the other methods update() and draw().  However, it is VERY important
+     * that we only quit AFTER a draw.
+     *
+     * @param delta Number of seconds since last animation frame
+     */
+    @Override
+    public void render(float delta) {
+        if (active) {
+            if (preUpdate(delta)) {
+                update(delta); // This is the one that must be defined.
+                postUpdate(delta);
+            }
+            draw(delta);
+        }
+    }
+
+    /**
+     * Called when the Screen is resized.
+     *
+     * This can happen at any point during a non-paused state but will never happen
+     * before a call to show().
+     *
+     * @param width  The new width in pixels
+     * @param height The new height in pixels
+     */
     @Override
     public void resize(int width, int height) {
 
     }
 
+    /**
+     * Called when the Screen is paused.
+     *
+     * This is usually when it's not active or visible on screen. An Application is
+     * also paused before it is destroyed.
+     */
     @Override
     public void pause() {
 
     }
 
-
-
+    /**
+     * Dispose of all (non-static) resources allocated to this mode.
+     */
     @Override
     public void dispose() {
-
+        levelModel.dispose();
+        canvas = null;
     }
 
     /**
@@ -187,13 +392,13 @@ public class WorldController implements Screen {
      *
      * This is usually when it regains focus.
      */
-    public void resume() {
-        // TODO Auto-generated method stub
-    }
+    @Override
+    public void resume() {}
 
     /**
      * Called when this screen becomes the current screen for a Game.
      */
+    @Override
     public void show() {
         // Useless if called in outside animation loop
         active = true;
@@ -202,8 +407,184 @@ public class WorldController implements Screen {
     /**
      * Called when this screen is no longer the current screen for a Game.
      */
+    @Override
     public void hide() {
         // Useless if called in outside animation loop
         active = false;
     }
+
+    /*
+    ******************************** PHYSICS ************************************/
+
+    /**
+     * Remove a new bullet from the world.
+     *
+     * @param  bullet   the bullet to remove
+     */
+    public void removeBullet(GameObject bullet) {
+        bullet.markRemoved(true);
+//        plopId = playSound( plopSound, plopId );
+    }
+
+    /**
+     * Callback method for the start of a collision
+     *
+     * This method is called when we first get a collision between two objects.  We use
+     * this method to test if it is the "right" kind of collision.  In particular, we
+     * use it to test if we made it to the win door.
+     *
+     * @param contact The two bodies that collided
+     */
+    @Override
+    public void beginContact(Contact contact) {
+        Fixture fix1 = contact.getFixtureA();
+        Fixture fix2 = contact.getFixtureB();
+
+        Body body1 = fix1.getBody();
+        Body body2 = fix2.getBody();
+
+        Object fd1 = fix1.getUserData();
+        Object fd2 = fix2.getUserData();
+
+        try {
+            GameObject bd1 = (GameObject)body1.getUserData();
+            GameObject bd2 = (GameObject)body2.getUserData();
+
+            // Test bullet collision with world
+            if (bd1.getName().equals("bullet") && bd2 != levelModel.raft) {
+                removeBullet(bd1);
+            }
+
+            if (bd2.getName().equals("bullet") && bd1 != levelModel.raft) {
+                removeBullet(bd2);
+            }
+
+//            // See if we have landed on the ground.
+//            if ((levelModel.avatar.getSensorName().equals(fd2) && levelModel.avatar != bd1) ||
+//                    (levelModel.avatar.getSensorName().equals(fd1) && levelModel.avatar != bd2)) {
+//                levelModel.avatar.setGrounded(true);
+//                sensorFixtures.add(levelModel.avatar == bd1 ? fix2 : fix1); // Could have more than one ground
+//            }
+
+            // Check for win condition
+            if ((bd1 == levelModel.raft && bd2 == levelModel.goal) ||
+                    (bd1 == levelModel.goal && bd2 == levelModel.raft)) {
+                setComplete(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Callback method for the start of a collision
+     *
+     * This method is called when two objects cease to touch.  The main use of this method
+     * is to determine when the characer is NOT on the ground.  This is how we prevent
+     * double jumping.
+     */
+    @Override
+    public void endContact(Contact contact) {
+//        Fixture fix1 = contact.getFixtureA();
+//        Fixture fix2 = contact.getFixtureB();
+//
+//        Body body1 = fix1.getBody();
+//        Body body2 = fix2.getBody();
+//
+//        Object fd1 = fix1.getUserData();
+//        Object fd2 = fix2.getUserData();
+//
+//        Object bd1 = body1.getUserData();
+//        Object bd2 = body2.getUserData();
+//
+//        if ((levelModel.avatar.getSensorName().equals(fd2) && levelModel.avatar != bd1) ||
+//                (levelModel.avatar.getSensorName().equals(fd1) && levelModel.avatar != bd2)) {
+//            sensorFixtures.remove(levelModel.avatar == bd1 ? fix2 : fix1);
+////            if (sensorFixtures.size == 0) {
+////                levelModel.avatar.setGrounded(false);
+////            }
+//        }
+    }
+
+    /** Unused ContactListener method */
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    /** Unused ContactListener method */
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+
+    /*
+     ******************************** Set and Reset Level ************************************/
+    /**
+     * Sets whether the level is completed.
+     *
+     * If true, the level will advance after a countdown
+     *
+     * @param value whether the level is completed.
+     */
+    public void setComplete(boolean value) {
+        if (value) {
+            countdown = EXIT_COUNT;
+        }
+        complete = value;
+    }
+
+    /**
+     * Returns true if the level is failed.
+     *
+     * If true, the level will reset after a countdown
+     *
+     * @return true if the level is failed.
+     */
+    public boolean isFailure( ) {
+        return failed;
+    }
+
+    /**
+     * Sets whether the level is failed.
+     *
+     * If true, the level will reset after a countdown
+     *
+     * @param value whether the level is failed.
+     */
+    public void setFailure(boolean value) {
+        if (value) {
+            countdown = EXIT_COUNT;
+        }
+        failed = value;
+    }
+
+    /**
+     * Resets the status of the game so that we can play again.
+     * <p>
+     * This method disposes of the world and creates a new one.
+     */
+    public void reset() {
+        levelModel.reset();
+        levelModel.world.setContactListener(this);
+        setComplete(false);
+        setFailure(false);
+        levelModel.populateLevel();
+    }
+
+    /**
+     * Populate the level according to the new level selection.
+     * <p>
+     * This method disposes of the world and creates a new one.
+     */
+    public void setLevel(int level_int){
+        levelModel.reset();
+        levelModel.world.setContactListener(this);
+        setComplete(false);
+        setFailure(false);
+        levelModel.setLevel(level_int);
+
+    }
+
 }
