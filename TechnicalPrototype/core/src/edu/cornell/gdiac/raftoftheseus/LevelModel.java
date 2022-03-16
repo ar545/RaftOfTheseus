83daec89942a83b373181e745255b62fba731daf
Leo Zhao <lz527@cornell.edu>
2022-03-16 15:53:36 -0400

    update JSON parser

--- TechnicalPrototype/core/src/edu/cornell/gdiac/raftoftheseus/LevelModel.java
+++ TechnicalPrototype/core/src/edu/cornell/gdiac/raftoftheseus/LevelModel.java
@@ -1,5 +1,6 @@
 package edu.cornell.gdiac.raftoftheseus;
 
+import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
@@ -9,7 +10,57 @@ import edu.cornell.gdiac.util.PooledList;
 
 public class LevelModel {
 
+    /*=*=*=*=*=*=*=*=*=* LEVEL CONSTANTS *=*=*=*=*=*=*=*=*=*/
+    /** Default width and height of a single grid in Box2d units */
+    private static final float DEFAULT_GIRD_EDGE_LENGTH = 3.0f;
+    /** Default boundary width and height of a single grid in Box2d units */
+    private static final float DEFAULT_BOUNDARY = 1.0f;
+    /** Default num of rows in the map (y, height) */
+    private static final int DEFAULT_GRID_ROW = 8;
+    /** Default num of columns in the map (x, width) */
+    private static final int DEFAULT_GRID_COL = 10;
+    /** Representing the length and width of a single gird unit on the board */
+    private static final Vector2 GRID_SIZE = new Vector2(DEFAULT_GIRD_EDGE_LENGTH, DEFAULT_GIRD_EDGE_LENGTH);
+    /** Top-down game with no gravity */
+    protected static final Vector2 NO_GRAVITY = new Vector2(0f ,0f );
+    /** This is used as a level int representing restarting the level */
+    protected static final int LEVEL_RESTART_CODE = -1;
 
+    /*=*=*=*=*=*=*=*=*=* TILED CONSTANTS *=*=*=*=*=*=*=*=*=*/
+    /** Index of the representation of default in tile set texture */
+    private static final int TILE_DEFAULT = 0;
+    /** Index of the representation of background in tile set texture */
+    private static final int TILE_BACKGROUND = 1;
+    /** Index of the representation of rock in tile set texture */
+    private static final int TILE_ROCK = 2;
+    /** Index of the representation of default treasure in tile set texture */
+    private static final int TILE_TREASURE = 3;
+    /** Index of the representation of goal in tile set texture */
+    private static final int TILE_GOAL = 4;
+    /** Index of the representation of default current in tile set texture */
+    private static final int TILE_CURRENT = 5;
+    /** Index of the representation of start in tile set texture */
+    private static final int TILE_START = 9;
+    /** Index of the representation of enemy in tile set texture */
+    private static final int TILE_ENEMY = 10;
+    /** Index of the representation of default wood in tile set texture */
+    private static final int TILE_WOOD = 11;
+    /** Offset of north current in tile set index */
+    private static final int TILE_NORTH = 0;
+    /** Offset of east current in tile set index */
+    private static final int TILE_EAST = 1;
+    /** Offset of south current in tile set index */
+    private static final int TILE_SOUTH = 2;
+    /** Offset of west current in tile set index */
+    private static final int TILE_WEST = 3;
+    /** layer of environment */
+    private static final int LAYER_ENV = 0;
+    /** layer of collectables */
+    private static final int LAYER_COL = 1;
+    /** layer of enemies */
+    private static final int LAYER_ENE = 2;
+
+    /*=*=*=*=*=*=*=*=*=* LEVEL FIELDS *=*=*=*=*=*=*=*=*=*/
     // TODO: class Raft/Player
     /** The player of the level */
     private GameObject raft;
@@ -17,51 +68,37 @@ public class LevelModel {
     /** The goal of the level */
     private GameObject goal;
     /** Reference to the game assets directory */
-    public AssetDirectory directory;
-    JsonValue level_data;
+    private AssetDirectory directory;
+    /** The read-in level data */
+    private JsonValue level_data;
     /** The Box2D world */
     protected World world;
     /** The boundary of the world */
     protected Rectangle bounds;
     /** The world scale */
-    protected Vector2 scale;
-    /** Representing the length and width of a single gird unit on the board */
-    protected Vector2 grid_size;
+    protected Vector2 scale = new Vector2(1, 1);
+    /** The map size in grid */
+    protected GridPoint2 map_size = new GridPoint2(DEFAULT_GRID_COL, DEFAULT_GRID_ROW);
     /** Vector 2 holding the temp position vector for the game object to create */
     private Vector2 compute_temp = new Vector2(0, 0);
-    /** Default width and height of a single grid in Box2d units */
-    protected static final float DEFAULT_GIRD_EDGE_LENGTH = 3.0f;
-    /** Default boundary width and height of a single grid in Box2d units */
-    protected static final float DEFAULT_BOUNDARY = 1.0f;
-    /** Default num of rows in the map (y, height) */
-    protected static final int DEFAULT_GRID_ROW = 6;
-    /** Default num of columns in the map (x, width) */
-    protected static final int DEFAULT_GRID_COL = 10;
-
-
-
-
     /** All the objects in the world. */
     private PooledList<GameObject> objects  = new PooledList<GameObject>();
     /** Queue for adding objects */
     private PooledList<GameObject> addQueue = new PooledList<GameObject>();
-    /** Top-down game with no gravity */
-    public static final Vector2 NO_GRAVITY = new Vector2(0f ,0f );
 
-    /** Constructor call for this singleton class */
-    public LevelModel(){}
-
+    /*=*=*=*=*=*=*=*=*=* INTERFACE: getter and setter *=*=*=*=*=*=*=*=*=*/
     /** get the reference to the player avatar */
     public GameObject getPlayer() { return raft; }
-
     /** get a reference to the goal */
     public GameObject getGoal() { return goal; }
-
     /** get the objects (list) of the world */
     public PooledList<GameObject> getObjects() { return objects; }
-
+    /***/
     public PooledList<GameObject> getAddQueue() { return addQueue; }
-
+    /** set directory */
+    protected void setDirectory(AssetDirectory directory) { this.directory = directory; }
+    /** Constructor call for this singleton class */
+    public LevelModel(){}
 
     /**
      * Returns true if the object is in bounds.
@@ -103,6 +140,8 @@ public class LevelModel {
         obj.activatePhysics(world);
     }
 
+    /*=*=*=*=*=*=*=*=*=* Level selection: dispose, select, and reset *=*=*=*=*=*=*=*=*=*/
+
     /**
      * Dispose of all (non-static) resources allocated to this mode.
      */
@@ -142,25 +181,45 @@ public class LevelModel {
      *
      * @param level_int an integer representing the level selection, i.e. which json file to read from. */
     public void loadLevel(int level_int){
-        level_data = directory.getEntry("levels:" + level_int, JsonValue.class);
-        setBound(level_data.get("bounds"));
+        if(level_int != LEVEL_RESTART_CODE){
+            // Load in new level
+            level_data = directory.getEntry("levels:" + level_int, JsonValue.class);
+
+            // Read in the grid map size
+            int num_col = level_data.getInt("width", DEFAULT_GRID_COL);
+            int num_row = level_data.getInt("height", DEFAULT_GRID_ROW);
+
+            // Reset boundary of world
+            setBound();
+        }
+        // Populate game objects
         populateLevel();
     }
 
     /** Set the physical boundary of the level. This boundary will be enforced when adding objects */
-    private void setBound(JsonValue world_data) {
-        int num_col = world_data.getInt("num_col", DEFAULT_GRID_COL);
-        int num_row = world_data.getInt("num_row", DEFAULT_GRID_ROW);
-        float col_width = world_data.getFloat("col_width", DEFAULT_GIRD_EDGE_LENGTH);
-        float row_height = world_data.getFloat("row_height", DEFAULT_GIRD_EDGE_LENGTH);
-        this.grid_size = new Vector2(col_width, row_height);
-        this.bounds = new Rectangle(0,0,col_width * num_col + 2 * DEFAULT_BOUNDARY,
-                row_height * num_row + 2 * DEFAULT_BOUNDARY);
-        this.scale = new Vector2(world_data.getFloat("draw_scale_x", 1),
-                world_data.getFloat("draw_scale_y", 1));
-        addWall(col_width * num_col, row_height * num_row);
+    private void setBound() {
+        // Read in the grid size and scale. This is not included in Tiled format
+//        float col_width = world_data.getFloat("col_width", DEFAULT_GIRD_EDGE_LENGTH);
+//        float row_height = world_data.getFloat("row_height", DEFAULT_GIRD_EDGE_LENGTH);
+//        this.grid_size = new Vector2(col_width, row_height);
+//        this.scale = new Vector2(world_data.getFloat("draw_scale_x", 1),
+//                world_data.getFloat("draw_scale_y", 1));
+
+        // Calculate the world bounds base on the grid map size
+        this.bounds = new Rectangle(0,0,GRID_SIZE.x * map_size.x + 2 * DEFAULT_BOUNDARY,
+                GRID_SIZE.y * map_size.y + 2 * DEFAULT_BOUNDARY);
+
+        // Add wall to the world
+        addWall(GRID_SIZE.x * map_size.x, GRID_SIZE.y * map_size.y);
     }
 
+    /**
+     * Add four wall (static objects) surrounding the box2d world to not let object go off-screen
+     * South wall rectangle: (0, 0, x+k, k)
+     * West wall rectangle: (0, k, k, y+2*k)
+     * North wall rectangle: (k, y+k, x+2*k, y+2*k)
+     * East wall rectangle: (x+k, 0, x+2*k, y+k)
+     * */
     private void addWall(float x, float y) {
 //        addObject(new Wall(0, 0, x+ LevelModel.DEFAULT_BOUNDARY, LevelModel.DEFAULT_BOUNDARY)); // GameObject south_wall
 //        addObject(new Wall(0, LevelModel.DEFAULT_BOUNDARY, LevelModel.DEFAULT_BOUNDARY, y+2* LevelModel.DEFAULT_BOUNDARY));  // GameObject west_wall
@@ -168,80 +227,142 @@ public class LevelModel {
 //        addObject(new Wall(x+ LevelModel.DEFAULT_BOUNDARY, 0, x+2* LevelModel.DEFAULT_BOUNDARY, y+ LevelModel.DEFAULT_BOUNDARY)); // GameObject east_wall
     }
 
-//    private void addWallAlternative(float x, float y, float k) {
-//        addObject(new Wall(0, 0, x+k, k)); // GameObject south_wall
-//        addObject(new Wall(0, k, k, y+2*k));  // GameObject west_wall
-//        addObject(new Wall(k, y+k, x+2*k, y+2*k)); // GameObject north_wall
-//        addObject(new Wall(x+k, 0, x+2*k, y+k)); // GameObject east_wall
-//    }
-
-    /** Populate the level with the game objects.
+    /**
+     * Populate the level with the game objects.
      * Precondition: gameObject list has been cleared. */
-    protected void populateLevel() {
-        addCurrent(level_data.get("current"));
-        addWood(level_data.get("wood"));
-        addRocks(level_data.get("rocks"));
-        addRaft(level_data.get("raft"));
-        addGoal(level_data.get("goal"));
+    private void populateLevel() {
+        JsonValue layers = level_data.get("layers");
+        JsonValue environment = layers.get(LAYER_ENV);
+        JsonValue collectables = layers.get(LAYER_COL);
+        JsonValue enemies = layers.get(LAYER_ENE);
 
+        // Loop through all index: for(int index = 0; index < map_size.x * map_size.y; index++)
+        for(int row = 0; row < map_size.y; row ++){
+            for(int col = 0; col < map_size.x; col ++){
+                int index = row * map_size.x + col;
+                populateEnv(row, col, environment.getInt(index));
+                populateCollect(row, col, collectables.getInt(index));
+                populateEnemies(row, col, enemies.getInt(index));
+            }
+        }
+    }
+
+    /***/
+    private void populateEnemies(int row, int col, int tile_int) {
+        switch (tile_int){
+            case TILE_ENEMY:
+                addEnemy(row, col);
+                break;
+            case TILE_START:
+                addRaft(row, col, 1f);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /***/
+    private void populateCollect(int row, int col, int tile_int) {
+        if(tile_int == TILE_TREASURE){
+            addTreasure(row, col);
+        } else if(tile_int >= TILE_WOOD){
+            addWood(row, col, tile_int - TILE_WOOD + 1);
+        }
+    }
+
+    /***/
+    private void populateEnv(int row, int col, int tile_int) {
+        if(tile_int == TILE_ROCK){
+            addRock(row, col);
+        }else if(tile_int == TILE_GOAL){
+            addGoal(row, col);
+        }else if(tile_int >= TILE_CURRENT && tile_int <= TILE_CURRENT + TILE_WEST){
+            addCurrent(row, col, tile_int - TILE_CURRENT, 1f);
+        }
+    }
+
+    /** Add Rock Objects to the world, using the Json value for goal.
+     * @param row the row gird position
+     * @param col the column grid position */
+    private void addRock(int row, int col) {
+        computePosition(col, row);
+//        GameObject this_rock = new Rock(compute_temp); addObject(this_rock);
+    }
+
+    /** Add Enemy Objects to the world, using the Json value for goal.
+     * @param row the row gird position
+     * @param col the column grid position */
+    private void addEnemy(int row, int col) {
+        computePosition(col, row);
+//        GameObject this_enemy = new Enemy(compute_temp); addObject(this_enemy);
+    }
+
+    /** Add Treasure Objects to the world, using the Json value for goal.
+     * @param row the row gird position
+     * @param col the column grid position */
+    private void addTreasure(int row, int col) {
+        computePosition(col, row);
+//        GameObject this_treasure = new Treasure(compute_temp); addObject(this_treasure);
     }
 
     /** Add Goal Objects to the world, using the Json value for goal.
-     * @param JSGoal the JS value that represents the goal */
-    private void addGoal(JsonValue JSGoal) {
-        computePosition(JSGoal.getInt("x", 0), JSGoal.getInt("y", 0));
+     * @param row the row gird position
+     * @param col the column grid position */
+    private void addGoal(int row, int col) {
+        computePosition(col, row);
 //        GameObject this_goal = new Goal(compute_temp);
 //        addObject(this_goal);
 //        goal = this_goal;
     }
 
     /** Add Raft Objects to the world, using the Json value for raft
-     * @param JSRaft the JS value that represents the raft */
-    private void addRaft(JsonValue JSRaft) {
-        computePosition(JSRaft.getInt("x", 0), JSRaft.getInt("y", 0));
-//        GameObject this_raft = new Raft(compute_temp, JSRaft.getInt("speed", 1));
+     * @param row the row gird position
+     * @param col the column grid position */
+    private void addRaft(int row, int col, float speed) {
+        computePosition(col, row);
+//        GameObject this_raft = new Raft(compute_temp, speed);
 //        addObject(this_raft);
 //        raft = this_raft;
     }
 
-    /** Add Goal Objects to the world, using the Json value for goal
-     * @param rocks the JS value that represents the goal */
-    private void addRocks(JsonValue rocks) {
-        for (JsonValue rock: rocks) {
-            computePosition(rock.getInt("x", 0), rock.getInt("y", 0));
-//            GameObject this_rock = new Rock(compute_temp);
-//            addObject(this_rock);
-        }
+    /** Add wood Objects to the world, using the Json value for goal
+     * @param row the row gird position
+     * @param col the column grid position
+     * @param value the JS value that represents the goal */
+    private void addWood(int row, int col, int value) {
+       computePosition(col, row);
+//       GameObject this_wood = new Wood(compute_temp, value);
+//       addObject(this_wood);
     }
 
-    /** Add Goal Objects to the world, using the Json value for goal
-     * @param woods the JS value that represents the goal */
-    private void addWood(JsonValue woods) {
-        for (JsonValue wood: woods) {
-            computePosition(wood.getInt("x", 0), wood.getInt("y", 0));
-//            GameObject this_wood = new Wood(compute_temp, wood.getInt("value", 1));
-//            addObject(this_wood);
-        }
+    /** Add current Objects to the world, using the Json value for goal
+     * @param row the row gird position
+     * @param col the column grid position
+     * @param direction the direction
+     * @param speed the speed */
+    private void addCurrent(int row, int col, int direction, float speed) {
+        computePosition(col, row);
+//        GameObject this_current = new Current(compute_temp, direction, speed);
+//        addObject(this_current);
     }
 
-    /** Add Goal Objects to the world, using the Json value for goal
-     * @param currents the JS value that represents the goal */
-    private void addCurrent(JsonValue currents) {
-        for (JsonValue current : currents) {
-            computePosition(current.getInt("x", 0), current.getInt("y", 0));
-//            GameObject this_current = new Current(compute_temp, current.getString("direction"), current.getInt("speed", 1));
-//            addObject(this_current);
-        }
-    }
-
-    /** compute the position of the object in the world given the grid location.
+    /** Compute the position of the object in the world given the grid location.
      * Result stored in compute_temp.
      * @param x_col the x grid value
      * @param y_row the y grid value */
     private void computePosition(int x_col, int y_row){
-        compute_temp.x = ((float) x_col + 0.5f) * grid_size.x;
-        compute_temp.y = ((float) y_row + 0.5f) * grid_size.y;
+        compute_temp.x = ((float) x_col + 0.5f) * GRID_SIZE.x;
+        compute_temp.y = ((float) y_row + 0.5f) * GRID_SIZE.y;
     }
 
+//    /** Add Goal Objects to the world, using the Json value for goal
+//     * @param rocks the JS value that represents the goal */
+//    private void addRocks(JsonValue rocks) {
+//        for (JsonValue rock: rocks) {
+//            computePosition(rock.getInt("x", 0), rock.getInt("y", 0));
+//            GameObject this_rock = new Rock(compute_temp);
+//            addObject(this_rock);
+//        }
+//    }
 
 }
