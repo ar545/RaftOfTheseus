2bb83dbcc7bcb6b37dfb80958b83fef285eb918e
Leo Zhao <lz527@cornell.edu>
2022-04-11 15:37:02 -0400

    level editor clean up

---
+++ TechnicalPrototype/core/assets/levels/RoT.tsx
@@ -0,0 +1,543 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="RoT" tilewidth="50" tileheight="50" tilecount="100" columns="10">
+ <image source="newTiled.png" width="500" height="500"/>
+ <tile id="0" type="Raft"/>
+ <tile id="1" type="Goal"/>
+ <tile id="2" type="Current">
+  <properties>
+   <property name="Direction" value="NE"/>
+   <property name="Magnitude" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3" type="Current">
+  <properties>
+   <property name="Direction" value="E"/>
+   <property name="Magnitude" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="4" type="Current">
+  <properties>
+   <property name="Direction" value="SE"/>
+   <property name="Magnitude" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="5" type="Current">
+  <properties>
+   <property name="Direction" value="S"/>
+   <property name="Magnitude" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6" type="Current">
+  <properties>
+   <property name="Direction" value="SW"/>
+   <property name="Magnitude" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="7" type="Current">
+  <properties>
+   <property name="Direction" value="W"/>
+   <property name="Magnitude" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8" type="Current">
+  <properties>
+   <property name="Direction" value="NW"/>
+   <property name="Magnitude" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9" type="Current">
+  <properties>
+   <property name="Direction" value="N"/>
+   <property name="Magnitude" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="10" type="Rock"/>
+ <tile id="11" type="Rock"/>
+ <tile id="12" type="Current">
+  <properties>
+   <property name="Direction" value="NE"/>
+   <property name="Magnitude" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="13" type="Current">
+  <properties>
+   <property name="Direction" value="E"/>
+   <property name="Magnitude" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="14" type="Current">
+  <properties>
+   <property name="Direction" value="SE"/>
+   <property name="Magnitude" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="15" type="Current">
+  <properties>
+   <property name="Direction" value="S"/>
+   <property name="Magnitude" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="16" type="Current">
+  <properties>
+   <property name="Direction" value="SW"/>
+   <property name="Magnitude" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="17" type="Current">
+  <properties>
+   <property name="Direction" value="W"/>
+   <property name="Magnitude" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="18" type="Current">
+  <properties>
+   <property name="Direction" value="NW"/>
+   <property name="Magnitude" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="19" type="Current">
+  <properties>
+   <property name="Direction" value="N"/>
+   <property name="Magnitude" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="20" type="Rock"/>
+ <tile id="21" type="Rock"/>
+ <tile id="22" type="Current">
+  <properties>
+   <property name="Direction" value="NE"/>
+   <property name="Magnitude" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="23" type="Current">
+  <properties>
+   <property name="Direction" value="E"/>
+   <property name="Magnitude" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="24" type="Current">
+  <properties>
+   <property name="Direction" value="SE"/>
+   <property name="Magnitude" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="25" type="Current">
+  <properties>
+   <property name="Direction" value="S"/>
+   <property name="Magnitude" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="26" type="Current">
+  <properties>
+   <property name="Direction" value="SW"/>
+   <property name="Magnitude" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="27" type="Current">
+  <properties>
+   <property name="Direction" value="W"/>
+   <property name="Magnitude" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="28" type="Current">
+  <properties>
+   <property name="Direction" value="NW"/>
+   <property name="Magnitude" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="29" type="Current">
+  <properties>
+   <property name="Direction" value="N"/>
+   <property name="Magnitude" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="30" type="Enemy">
+  <properties>
+   <property name="Type" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="31" type="Enemy">
+  <properties>
+   <property name="Type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="32" type="Current">
+  <properties>
+   <property name="Direction" value="NE"/>
+   <property name="Magnitude" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="33" type="Current">
+  <properties>
+   <property name="Direction" value="E"/>
+   <property name="Magnitude" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="34" type="Current">
+  <properties>
+   <property name="Direction" value="SE"/>
+   <property name="Magnitude" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="35" type="Current">
+  <properties>
+   <property name="Direction" value="S"/>
+   <property name="Magnitude" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="36" type="Current">
+  <properties>
+   <property name="Direction" value="SW"/>
+   <property name="Magnitude" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="37" type="Current">
+  <properties>
+   <property name="Direction" value="W"/>
+   <property name="Magnitude" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="38" type="Current">
+  <properties>
+   <property name="Direction" value="NW"/>
+   <property name="Magnitude" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="39" type="Current">
+  <properties>
+   <property name="Direction" value="N"/>
+   <property name="Magnitude" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="40" type="Enemy">
+  <properties>
+   <property name="Type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="41" type="Enemy">
+  <properties>
+   <property name="Type" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="42" type="Current">
+  <properties>
+   <property name="Direction" value="NE"/>
+   <property name="Magnitude" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="43" type="Current">
+  <properties>
+   <property name="Direction" value="E"/>
+   <property name="Magnitude" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="44" type="Current">
+  <properties>
+   <property name="Direction" value="SE"/>
+   <property name="Magnitude" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="45" type="Current">
+  <properties>
+   <property name="Direction" value="S"/>
+   <property name="Magnitude" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="46" type="Current">
+  <properties>
+   <property name="Direction" value="SW"/>
+   <property name="Magnitude" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="47" type="Current">
+  <properties>
+   <property name="Direction" value="W"/>
+   <property name="Magnitude" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="48" type="Current">
+  <properties>
+   <property name="Direction" value="NW"/>
+   <property name="Magnitude" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="49" type="Current">
+  <properties>
+   <property name="Direction" value="N"/>
+   <property name="Magnitude" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="50" type="Treasure"/>
+ <tile id="51" type="Treasure"/>
+ <tile id="52" type="Current">
+  <properties>
+   <property name="Direction" value="NE"/>
+   <property name="Magnitude" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="53" type="Current">
+  <properties>
+   <property name="Direction" value="E"/>
+   <property name="Magnitude" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="54" type="Current">
+  <properties>
+   <property name="Direction" value="SE"/>
+   <property name="Magnitude" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="55" type="Current">
+  <properties>
+   <property name="Direction" value="S"/>
+   <property name="Magnitude" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="56" type="Current">
+  <properties>
+   <property name="Direction" value="SW"/>
+   <property name="Magnitude" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="57" type="Current">
+  <properties>
+   <property name="Direction" value="W"/>
+   <property name="Magnitude" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="58" type="Current">
+  <properties>
+   <property name="Direction" value="NW"/>
+   <property name="Magnitude" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="59" type="Current">
+  <properties>
+   <property name="Direction" value="N"/>
+   <property name="Magnitude" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="60" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="61" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="62" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="63" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="64" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="65" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="66" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="67" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="8"/>
+  </properties>
+ </tile>
+ <tile id="68" type="Wood">
+  <properties>
+   <property name="Value" type="int" value="9"/>
+  </properties>
+ </tile>
+ <tile id="69" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="70" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="71" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="72" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="73" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="74" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="75" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="76" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="77" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="8"/>
+  </properties>
+ </tile>
+ <tile id="78" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="1"/>
+   <property name="Value" type="int" value="9"/>
+  </properties>
+ </tile>
+ <tile id="79" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="80" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="81" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="82" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="83" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="84" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="85" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="86" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="87" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="8"/>
+  </properties>
+ </tile>
+ <tile id="88" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="2"/>
+   <property name="Value" type="int" value="9"/>
+  </properties>
+ </tile>
+ <tile id="89" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="90" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="3"/>
+   <property name="Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="91" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="92" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="4"/>
+   <property name="Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="93" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="94" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="5"/>
+   <property name="Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="95" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="96" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="6"/>
+   <property name="Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="97" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="98" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="7"/>
+   <property name="Value" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="99" type="Wood">
+  <properties>
+   <property name="Tens of Value" type="int" value="8"/>
+  </properties>
+ </tile>
+</tileset>
